dec387c391aefb1a017c2514c59c7ba6b3aebfd7
killgriff22 <kilgrif22033@gmail.com>
2023-09-21 15:50:41 -0400

    revamped selection system fixed bugs

--- engine.py
+++ engine.py
@@ -28,39 +28,17 @@ def build():
             Banks.append(engine_generator.Bank(bank,offsets[i]))
         engine = engine_generator.Engine(Banks, cylinders)
     items = ['engine_name','stroke','bore','chamber_volume','starter_torque','starter_speed','idle_throttle_plate_position','rod_length','simulation_frequency','redline','rev_limit','intake_flow_rate']
-    for i,item in enumerate(items):
-        print(f"{i+1}. change {item} ({getattr(engine,item)})")
-    inp = input(">>")
+    inp = "0"
     while not inp == "" and not int(inp) == len(items)+1:
         for i,item in enumerate(items):
             print(f"{i+1}. change {item} ({getattr(engine,item)})")
-        print(f"{len(items)+1}. generate")
-        inp=int(inp)
-        if inp == 1:
-            engine.engine_name = input("enter engine name\n>>")
-        elif inp == 2:
-            engine.stroke = float(input("enter stroke\n>>"))
-        elif inp == 3:
-            engine.bore = float(input("enter bore\n>>"))
-        elif inp == 4:
-            engine.chamber_volume = float(input("enter chamber volume\n>>"))
-        elif inp == 5:
-            engine.starter_torque = float(input("enter starter torque\n>>"))
-        elif inp == 6:
-            engine.starter_speed = float(input("enter starter speed\n>>"))
-        elif inp == 7:
-            engine.idle_throttle_plate_position = float(input("enter idle throttle plate position\n>>"))
-        elif inp == 8:
-            engine.rod_length = float(input("enter rod length\n>>"))
-        elif inp == 9:
-            engine.simulation_frequency = int(input("enter simulation frequency\n>>"))
-        elif inp == 10:
-            engine.redline = int(input("enter redline\n>>"))
-        elif inp == 11:
-            engine.rev_limit = int(input("enter rev limit\n>>"))
-        elif inp == 12:
-            engine.intake_flow_rate = int(input("enter intake flow rate\n>>"))
         inp = input(">>")
+        if inp == "":
+            break
+        for i,item in enumerate(items):
+            if inp == str(i+1):
+                setattr(engine,item,input(f"enter new {item} ({getattr(engine,item)})\n>>"))
+                break
     name = input(f"enter engine name {engine.engine_name}\n>>")
     if name:
         engine.engine_name = name
